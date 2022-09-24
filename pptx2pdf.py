6b027a2e0f1c828717daee4cefb73bf01d94387a
TrainedPro <hassaananwar12345@gmail.com>
2022-09-24 20:33:19 +0500

    Updated Some Lines To Make It Better

--- pptx2pdf.py
+++ pptx2pdf.py
@@ -8,18 +8,20 @@ os.chdir(inputfldr)
 
 print('Directory Changed To:', inputfldr)
 
-print(f'ppt2pdf dir .')
 os.system(f'ppt2pdf dir .')
 
+# List To Delete All The Individual ".pdf" Files
 dellst = []
 
 merger = PdfFileMerger()
+
+# Loop To Gather All ".pdf" Files In The inputflder
 for pptx in glob.iglob(inputfldr + '/*.pdf'):
     merger.append(pptx)
     dellst.append(pptx)
 
 # Will Merge All The PDFs
-merger.write(f"{inputfldr}/merged_all_pages.pdf")
+merger.write(f"{inputfldr}/MergedDocument.pdf")
 merger.close()
 
 # Will Delete The Individual PDFs
